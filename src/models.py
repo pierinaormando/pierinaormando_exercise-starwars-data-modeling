2b26d8f6c4f070df46591cadf9e157c3ea3af0cb
Pierina Ormando <110559878+pierinaormando@users.noreply.github.com>
2023-12-15 20:24:26 +0000

    corrected relationships and added users table

--- src/models.py
+++ src/models.py
@@ -7,6 +7,12 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
+class User(Base):
+    __tablename__= 'user'
+    id = Column(Integer, primary_key=True)
+    name = Column(String (40), unique=True)
+    email = Column(String (100))
+
 class Planet(Base):
     __tablename__= 'planet'
     id = Column(Integer, primary_key=True)
@@ -30,29 +36,35 @@ class Vehicle(Base):
     model = Column(String(40))
     passenger = Column(Integer)
 
-class VehiclePilots(Base):
-    __tablename__= 'vehiclePilots'
+class Vehicle_Pilots(Base):
+    __tablename__= 'vehicle_pilots'
     id = Column(Integer, primary_key=True)
     character_id = Column(Integer, ForeignKey('character.id'))
-    vehicle_id = Column(Integer, ForeignKey('vehicle.id'))
     character = relationship(Character)
-    vehicle = relationship(Vehicle)
-
-class FavoritesVehicles(Base):
-    __tablename__= 'favoritesVehicles'
-    id = Column(Integer, primary_key=True)
     vehicle_id = Column(Integer, ForeignKey('vehicle.id'))
     vehicle = relationship(Vehicle)
 
-class FavoritesPlanets(Base):
-    __tablename__= 'favoritesPlanets'
+class Favorite_Vehicle(Base):
+    __tablename__= 'favorite_vehicle'
     id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
+    vehicle_id = Column(Integer, ForeignKey('vehicle.id'))
+    vehicle = relationship(Vehicle)
+
+class Favorite_Planet(Base):
+    __tablename__= 'favorite_planet'
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
     planet_id = Column(Integer, ForeignKey('planet.id'))
     planet = relationship(Planet)
 
-class FavoritesCharacters(Base):
-    __tablename__= 'favoritesCharacters'
+class Favorite_Character(Base):
+    __tablename__= 'favorite_character'
     id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
     character_id = Column(Integer, ForeignKey('character.id'))
     character = relationship(Character)
     
